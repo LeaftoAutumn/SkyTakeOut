7006204cce5a97724a151f543e8755e48c9957ab
Leaf <leaf.autumn.386445@gmail.com>
2025-05-07 17:17:10 +0800

    工作台相关代码开发。

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -12,6 +12,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface SetmealMapper {
@@ -85,4 +86,11 @@ public interface SetmealMapper {
      */
     @Select("select * from setmeal where category_id = #{id}")
     List<Setmeal> listByCategoryId(Long id);
+
+    /**
+     * 根据条件统计套餐数量
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
